ac0a751da350bbac8669c6da5dea92af7742a625
Al Jay Wenceslao <aljaywenceslao12@gmail.com>
2026-02-01 21:23:40 +0800

    Implement getLanguageInfo to return language info string and add getIdentity method returning language description

--- Language.java
+++ Language.java
@@ -6,4 +6,10 @@ public class Language{
         this.language_name = language_name;
         this.speakers = speakers;
     }
+    public String getLanguageInfo() {
+        return language_name + ", " + speakers;
+    }
+    public String getIdentity() {
+        return "I am the Language Class. I hold details about the primary tongue of the nation.";
+    }
 }
