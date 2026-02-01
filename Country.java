7208f8c0f4e0ca1a06e283e9d802466ae5d12f15
gc.bigcas <gc.bigcas@gmail.com>
2026-02-01 23:34:48 +0800

    Adds country model
    Adds a country model with relationships to city and language.
    
    This introduces a structured way to manage country-specific data,
    including its major city and official language.

--- Country.java
+++ Country.java
@@ -1,3 +1,11 @@
 public class Country {
-    
+    private String country_name;
+    private City city;
+    private Language language;
+
+    public Country(String country_name, String city_name, int city_pop, Language language) {
+        this.country_name = country_name;
+        this.city = new City(city_name, city_pop);
+        this.language = language;
+    }
 }
