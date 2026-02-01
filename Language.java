f64e9c4ed9260fa0d42069a0525d3ffc86a9470d
Al Jay Wenceslao <aljaywenceslao12@gmail.com>
2026-02-01 21:19:02 +0800

    Defines Language class
    Adds a Language class with name and speaker count.
    The class stores the language name and the number of speakers.

--- Language.java
+++ Language.java
@@ -1,3 +1,9 @@
 public class Language{
-    
+    private String language_name;
+    private int speakers;
+
+    public Language(String language_name, int speakers) {
+        this.language_name = language_name;
+        this.speakers = speakers;
+    }
 }
