96d5734777c468f22a312133e8ec10b4b41a60d7
gc.bigcas <gc.bigcas@gmail.com>
2026-02-02 10:01:50 +0800

    Adds country information printing
    Creates Country and Language objects and prints the country information.
    This allows the program to display information about a specific country,
    including its name, capital, population, and language.

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
   public static void main(String[] args) {
     
+    Language filipino = new Language("Filipino", 90000000);
+
+        Country ph = new Country("Philippines", "Manila", 19000000, filipino);
+
+        System.out.println(ph.getCountryInfo());
   }
 }
